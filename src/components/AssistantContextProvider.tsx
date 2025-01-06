1f546a14d70edbbc4ec475927eceff1bdad6682f
Kit G. <inariwind@gmail.com>
2025-01-06 14:43:37 -0500

    refactor: context types

--- src/components/AssistantContextProvider.tsx
+++ src/components/AssistantContextProvider.tsx
@@ -1,15 +1,18 @@
-import { AssistantContextProviderProps } from "@constants/interfaces";
 import { createContext } from "react";
 import { useChat } from "ai/react";
+import {
+  AssistantContextProviderProps,
+  AssistantContextType,
+} from "@constants/interfaces";
 
 /** This component is the app's context with all the chat data. */
 
-export const AssistantContext = createContext({
+export const AssistantContext = createContext<AssistantContextType>({
   messages: [],
   input: "",
   handleInputChange: () => null,
   handleSubmit: () => null,
-  append: (p0: { content: string; role: string }) => null,
+  append: () => null,
 });
 
 export default function AssistantContextProvider({
@@ -18,8 +21,7 @@ export default function AssistantContextProvider({
   const chatData = useChat();
 
   return (
-    // FIXME: type
-    <AssistantContext.Provider value={chatData as never}>
+    <AssistantContext.Provider value={chatData as AssistantContextType}>
       {children}
     </AssistantContext.Provider>
   );
